39f841859587d7d396eb6ee51ca8f3a3e5c50660
kiethohoai <kietho.060193@gmail.com>
2024-10-29 09:29:37 +0700

    005 Our First AJAX Call XMLHttpRequest

---
+++ script.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+const getCountryData = country => {
+  const request = new XMLHttpRequest();
+  request.open(
+    'GET',
+    `https://restcountries.com/v3.1/name/${country}?fullText=true`
+  );
+  request.send();
+
+  request.addEventListener('load', function () {
+    const res = JSON.parse(this.responseText);
+    const data = res[0];
+
+    const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
+        <p class="country__row"><span>💰</span>${data.currencies.EUR.name}${
+      data.currencies.EUR.symbol
+    }</p>
+      </div>
+    </article>
+  `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+const getCountryData2 = country => {
+  const request = new XMLHttpRequest();
+  request.open(
+    'GET',
+    `https://restcountries.com/v3.1/name/${country}?fullText=true`
+  );
+  request.send();
+
+  request.addEventListener('load', function () {
+    const res = JSON.parse(this.responseText);
+    const data = res[0];
+
+    const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages.vie}</p>
+        <p class="country__row"><span>💰</span>${data.currencies.VND.name}${
+      data.currencies.VND.symbol
+    }</p>
+      </div>
+    </article>
+  `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('portugal');
+getCountryData2('vietnam');
