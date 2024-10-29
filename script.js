c4334eb2a8ed233df45d0257180720f9e4676128
kiethohoai <kietho.060193@gmail.com>
2024-10-29 14:11:33 +0700

    009 Consuming Promises

--- script.js
+++ script.js
@@ -138,5 +138,48 @@ const getCountryAndNeigbour = country => {
 getCountryAndNeigbour('portugal');
  */
 
-const res = fetch(`https://restcountries.com/v3.1/name/portugal`);
-console.log(`🚀  res =>`, res.json());
+// const res = fetch(`https://restcountries.com/v3.1/name/portugal`);
+// console.log(`🚀  res =>`, res.json());
+
+const renderCountry = (data, className) => {
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
+        <p class="country__row"><span>💰</span>${data.currencies.EUR.name} ${
+    data.currencies.EUR.symbol
+  }</p>
+      </div>
+    </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(function (respone) {
+      console.log(`🚀  respone =>`, respone);
+      return respone.json();
+    })
+    .then((data) => {
+      console.log(`🚀  data =>`, data);
+      renderCountry(data[0], '');
+    });
+};
+
+const getCountryData2 = (country) => {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then((respone) => respone.json())
+    .then((data) => renderCountry(data[0], ''));
+};
+
+getCountryData('portugal');
+getCountryData2('portugal');
