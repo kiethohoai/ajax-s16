ce036cb22c3611ede80b9f1f0fa8beda8437dcbb
kiethohoai <kietho.060193@gmail.com>
2024-10-30 14:53:49 +0700

    005 Exporting and Importing in ES6 Modules

---
+++ shoppingCart.js
@@ -0,0 +1,14 @@
+const shippingCost = 10;
+const cart = [];
+
+export const addToCart = (product, quantity) => {
+  cart.push({ product, quantity });
+  console.log(`This ${product} with ${quantity} quantities was added to the cart`);
+};
+
+const totalPrice = 1000;
+const totalQuantity = 10;
+
+export { totalPrice, totalQuantity };
+
+export default addToCart;
